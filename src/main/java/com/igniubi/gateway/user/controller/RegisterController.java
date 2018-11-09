70c47d7aeda67cf26b0202713e85ba1ef7f3beca
shuai.zhang <shuai.zhang@yoho.cn>
2018-11-09 16:33:31 +0800

    修改

--- src/main/java/com/igniubi/gateway/user/controller/RegisterController.java
+++ src/main/java/com/igniubi/gateway/user/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.igniubi.gateway.user.controller;
 import com.igniubi.gateway.common.ServerConstant;
 import com.igniubi.model.CommonRsp;
 import com.igniubi.model.user.request.RegisterReq;
+import com.igniubi.rest.client.RestServiceCaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class RegisterController {
@@ -18,7 +19,9 @@ public class RegisterController {
     private final Logger logger = LoggerFactory.getLogger(RegisterController.class);
 
     @Autowired
-    RestTemplate restTemplate;
+    RestServiceCaller serviceCaller;
+
+    private static String REGISTER_URL = "user/registerRest/register";
 
     @RequestMapping("/register")
     @ResponseBody
@@ -27,7 +30,7 @@ public class RegisterController {
         RegisterReq req = new RegisterReq();
         req.setMobile(mobile);
         req.setPassword(password);
-        CommonRsp result = restTemplate.postForObject(ServerConstant.USER+ "user/registerRest/register",req,CommonRsp.class);
+        CommonRsp result = serviceCaller.call(ServerConstant.USER,  REGISTER_URL,req,CommonRsp.class);
         return result;
     }
 }
