5b9626852640997ca65668931598332cce38acc1
shuai.zhang <shuai.zhang@yoho.cn>
2018-12-04 17:14:54 +0800

    允许跨域

--- src/main/java/com/igniubi/gateway/Intercepter/IntercepterConfiguration.java
+++ src/main/java/com/igniubi/gateway/Intercepter/IntercepterConfiguration.java
@@ -2,8 +2,10 @@ package com.igniubi.gateway.Intercepter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class IntercepterConfiguration extends WebMvcConfigurationSupport {
@@ -17,6 +19,13 @@ public class IntercepterConfiguration extends WebMvcConfigurationSupport {
 
     }
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE");
+    }
 
     @Bean
     public AuthIntercepter accessIntercepter(){
