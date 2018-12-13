9fce052273ac6e41e4b8d0416c97f144563ba7f2
shuai.zhang <shuai.zhang@yoho.cn>
2018-12-13 14:36:22 +0800

    v2

---
+++ src/main/java/com/igniubi/gateway/article/controller/ArticleController.java
@@ -0,0 +1,42 @@
+package com.igniubi.gateway.article.controller;
+
+import com.igniubi.gateway.common.ServerConstant;
+import com.igniubi.model.CommonRsp;
+import com.igniubi.model.article.req.ArticleReq;
+import com.igniubi.model.article.rsp.ArticleRsp;
+import com.igniubi.model.user.req.RegisterReqBO;
+import com.igniubi.model.user.req.UserProfileReqBO;
+import com.igniubi.rest.client.AsyncFuture;
+import com.igniubi.rest.client.RestServiceCaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class ArticleController {
+
+    private final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+
+    @Autowired
+    RestServiceCaller serviceCaller;
+
+    private static String INDEX_URL = "/getIndex";
+
+
+    @RequestMapping("/articleIndex")
+    public CommonRsp articleIndex(@RequestParam("date") String date){
+        ArticleReq req = new ArticleReq();
+        req.setDate(date);
+        List list = serviceCaller.call(ServerConstant.ARTICLE, INDEX_URL, req, List.class);
+        return new CommonRsp.CommonrspBuilder().data(list).build();
+    }
+
+
+}
--- src/main/java/com/igniubi/gateway/common/ServerConstant.java
+++ src/main/java/com/igniubi/gateway/common/ServerConstant.java
@@ -3,4 +3,6 @@ package com.igniubi.gateway.common;
 public class ServerConstant {
 
     public static final String USER = "http://USER/user/";
+
+    public static final String ARTICLE = "http://ARTICLE/article/";
 }
