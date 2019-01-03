fb82294ffff6e6bac7e40c9d3bcd0505a04a2db5
shuai.zhang <shuai.zhang@yoho.cn>
2019-01-03 16:38:07 +0800

    auth

--- src/main/java/com/igniubi/gateway/GatewayApplication.java
+++ src/main/java/com/igniubi/gateway/GatewayApplication.java
@@ -5,16 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @ComponentScan(basePackages = "com.igniubi")
+@RestController
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @RequestMapping("/health")
+    public String health(){
+
+        return "success";
+    }
+    @RequestMapping("/")
+    public String index(){
+
+        return "welcome!";
+    }
 
 }
--- src/main/java/com/igniubi/gateway/Intercepter/AuthIntercepter.java
+++ src/main/java/com/igniubi/gateway/Intercepter/AuthIntercepter.java
@@ -1,15 +1,20 @@
 package com.igniubi.gateway.Intercepter;
 
+import com.igniubi.common.exceptions.IGNBException;
 import com.igniubi.gateway.common.ServerConstant;
+import com.igniubi.model.enums.common.ResultEnum;
 import com.igniubi.model.user.req.SessionReqBO;
 import com.igniubi.redis.operations.RedisValueOperations;
 import com.igniubi.redis.util.RedisKeyBuilder;
 import com.igniubi.rest.client.RestServiceCaller;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Enumeration;
@@ -38,10 +43,25 @@ public class AuthIntercepter implements HandlerInterceptor {
         if (params == null){
             return true;
         }
-        String sessionKey = params.get("session");
+        Cookie[] cookies = request.getCookies();
+        String sessionKey = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                //解析sessionid
+                if ("JSESSIONID".equals(cookie.getName())) {
+                    sessionKey = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
         String uid = params.get("uid");
-        if(sessionKey ==null || uid ==null){
+        if(uid ==null){
             return true;
+        }
+        if(StringUtils.isEmpty(sessionKey)){
+            logger.info("AuthIntercepter user session failed , sessionkey in cookies is null");
+            throw new IGNBException(ResultEnum.SESSION_FAIL);
         }
         return valiteSession(sessionKey,uid);
     }
@@ -73,8 +93,12 @@ public class AuthIntercepter implements HandlerInterceptor {
             SessionReqBO reqBO = new SessionReqBO();
             reqBO.setSessionKey(sessionKey);
             uidinfo = serviceCaller.call(ServerConstant.USER,"session/getUid",reqBO,String.class);
-            logger.info("AuthIntercepter getuid from user, sessionkey is {}, uid is {}", sessionKey, uidinfo);
+            logger.info("AuthIntercepter getuid from user, sessionkey is {}, uidinfo is {}", sessionKey, uidinfo);
         }
-        return uid.equals(uidinfo);
+        if(!uid.equals(uidinfo)){
+            logger.info("AuthIntercepter user session failed , sessionkey is {}, uid is {}, uidinfo is{} ", sessionKey, uid, uidinfo);
+            throw new IGNBException(ResultEnum.SESSION_FAIL);
+        }
+        return true;
     }
 }
