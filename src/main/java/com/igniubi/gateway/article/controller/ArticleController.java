32a08eb449365ac84573e63f07c16950f61f8d45
shuai.zhang <shuai.zhang@yoho.cn>
2019-11-27 09:41:56 +0800

    。。

--- src/main/java/com/igniubi/gateway/article/controller/ArticleController.java
+++ src/main/java/com/igniubi/gateway/article/controller/ArticleController.java
@@ -5,17 +5,31 @@ import com.igniubi.model.CommonRsp;
 import com.igniubi.model.article.req.ArticleReq;
 import com.igniubi.rest.client.AsyncResult;
 import com.igniubi.rest.client.RestClientCaller;
-import com.igniubi.rest.client.RestServiceCaller;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.model.CannedAccessControlList;
+import com.qcloud.cos.model.CreateBucketRequest;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.region.Region;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
 
 
 @RestController
@@ -87,4 +101,64 @@ public class ArticleController {
         return new CommonRsp.CommonrspBuilder().data(result).build();
     }
 
+    @RequestMapping("/upload")
+    @ResponseBody
+    public CommonRsp grassFileUpload(@RequestParam("file") MultipartFile[] file
+                                      )throws Exception{
+        CommonRsp responseBean = new CommonRsp();
+        String fileUrl = file[0].getOriginalFilename();
+        String fileName = fileUrl.indexOf("/") > 0 ? fileUrl.substring(fileUrl.lastIndexOf("/")) : fileUrl ;
+        fileName = DigestUtils.md5Hex(new Date().getTime() + fileName) +
+                "." + fileName.substring(fileName.lastIndexOf(".") + 1);
+
+        String tempDir = System.getProperty("java.io.tmpdir");
+        String localAnimatedUrl = tempDir +File.separator+ fileName;
+
+        File file1 = new File(localAnimatedUrl);
+        file[0].transferTo(file1);
+        String url = this.uploadToTencent(file1, fileName);
+        responseBean.setCode(200);
+        responseBean.setMessage("上传成功");
+        responseBean.setData(url);
+        return responseBean;
+    }
+
+    private String uploadToTencent(File file , String fileName) {
+        // 1 初始化用户身份信息（secretId, secretKey）。
+        COSCredentials cred = new BasicCOSCredentials("AKID94kANP7tHPIwLU46ZnlQq1xe8MPEmNaX", "sWu268vUotjHZcWGm46tSnsnUxBBwRhh");
+        // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+        // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
+        Region region = new Region("ap-shanghai");
+        ClientConfig clientConfig = new ClientConfig(region);
+        // 3 生成 cos 客户端。
+        COSClient cosClient = new COSClient(cred, clientConfig);
+        // 创建存储桶
+        String bucketName = "01-1253461321"; //存储桶名称，格式：BucketName-APPID
+        CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketName);
+        // 设置 bucket 的权限为 PublicRead(公有读私有写), 其他可选有私有读写, 公有读写
+        createBucketRequest.setCannedAcl(CannedAccessControlList.PublicRead);
+        try{
+            if(!cosClient.doesBucketExist(bucketName)){
+                cosClient.createBucket(createBucketRequest);
+            }
+            // 指定要上传的文件
+//            File localFile = new File(fileUrl);
+            // 指定要上传到的存储桶
+            // 指定要上传到 COS 上对象键
+            String key =  genFileName(fileName);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, file);
+            cosClient.putObject(putObjectRequest);
+            return "https://01-1253461321.cos.ap-shanghai.myqcloud.com" + key;
+        } catch (Exception e) {
+            logger.error("downloadPicture error , the url is {} , the error is {}",fileName , e);
+            throw e;
+        } finally {
+            cosClient.shutdown();
+        }
+    }
+
+    private String genFileName(String oriName){
+        String saveName = new SimpleDateFormat("/yyyy/MM/dd/HH/").format(new Date());
+        return saveName+ oriName;
+    }
 }
